4a391eb8a5496d4a5a0f1d6db782bdb805254c77
Alagupand-25 <142714515+Alagupand-25@users.noreply.github.com>
2025-05-30 10:00:16 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
