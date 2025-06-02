37454e90cb27edb6eda8e84e18e80a48fb594b86
Alagupand-25 <142714515+Alagupand-25@users.noreply.github.com>
2025-06-02 19:28:22 +0530

    implemented db connection

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+const dbConnection = require('./config/db');
+
 app.use(cors());
 app.use(bodyParser.json());
+
+dbConnection();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
